18bbc1faaf11a26177a71456ad11a1e63788dd00
Ferenczi Samu <farkas.david05@gmail.com>
2018-04-26 13:44:58 +0200

    started

---
+++ src/main/java/com/greenfox/complains/ComplainsApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.complains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplainsApplication.class, args);
+	}
+}
